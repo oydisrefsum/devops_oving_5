18e387f30039b3b16ee2f1aafa45046ca60ce193
Øydis Kind Refsum <oeydis@gmail.com>
2021-09-28 10:26:56 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/devops_oving_5/DevopsOving5Application.kt
@@ -0,0 +1,11 @@
+package com.example.devops_oving_5
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevopsOving5Application
+
+fun main(args: Array<String>) {
+    runApplication<DevopsOving5Application>(*args)
+}
---
+++ src/test/kotlin/com/example/devops_oving_5/DevopsOving5ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.devops_oving_5
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevopsOving5ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
